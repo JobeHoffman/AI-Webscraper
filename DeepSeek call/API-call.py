718ab87c3923b0b2d7c2d12fc4ec83780c01219e
Jobe Hoffman <sblancha@andrew.cmu.edu>
2025-05-14 22:15:14 -0400

    Update API-call.py

--- DeepSeek call/API-call.py
+++ DeepSeek call/API-call.py
@@ -3,10 +3,9 @@ from openai import OpenAI
 client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
-    model="deepseek-chat",
+    model="deepseek-reasoner",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
+        {"role": "user", "content": f'{prompt}'},
     ],
     stream=False
 )
